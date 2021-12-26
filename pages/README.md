b591aa64737cf9dcb0f497c2115712c51fb06303
tomox0115 <it0731@outlook.jp>
2021-12-27 03:35:34 +0900

    cahnge dir

---
+++ pages/README.md
@@ -0,0 +1,30 @@
+Home Page(改修予定)
+
+https://tools.ic731.net/NewFileShortcut/
+
+```
+pages
+│  index.html
+│  main.js
+│  README.md
+│  style.css
+│
+├─assets
+│      cover.webp
+│      nfs.ico
+│      ogp.jpg
+│
+├─docs
+│      release.md
+│      top.md
+│      tutorial.md
+│      vkey.md
+│
+└─vue
+        release.vue
+        top.vue
+        tutorial.vue
+        vkey.vue
+
+```
+
--- pages/main.js
+++ pages/main.js
@@ -1,10 +1,9 @@
 const router = new VueRouter({
-    mode: 'history',
     base: '/NewFileShortcut/',
     routes: [
         { path: '/', component: httpVueLoader('./vue/top.vue') },
         { path: '/tutorial', component: httpVueLoader('./vue/tutorial.vue') },
-        { path: '/release-log', component: httpVueLoader('./vue/release.vue') },
+        { path: '/release', component: httpVueLoader('./vue/release.vue') },
         { path: '/vkey', component: httpVueLoader('./vue/vkey.vue') }
     ]
 })
