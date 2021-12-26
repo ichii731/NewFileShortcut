c42863c2e86816d37d1f8ecc534c43a623fe2777
tomox0115 <it0731@outlook.jp>
2021-12-27 02:42:38 +0900

    Vur router

--- docs/main.js
+++ docs/main.js
@@ -1,10 +1,10 @@
 const router = new VueRouter({
     mode: 'history',
     routes: [
-        { path: '/', component: httpVueLoader('./vue/top.vue') },
-        { path: '/tutorial', component: httpVueLoader('./vue/tutorial.vue') },
-        { path: '/release', component: httpVueLoader('./vue/release.vue') },
-        { path: '/vkey', component: httpVueLoader('./vue/vkey.vue') }
+        { path: 'NewFileShortcut/', component: httpVueLoader('./vue/top.vue') },
+        { path: 'NewFileShortcut/tutorial', component: httpVueLoader('./vue/tutorial.vue') },
+        { path: 'NewFileShortcut/release', component: httpVueLoader('./vue/release.vue') },
+        { path: 'NewFileShortcut/vkey', component: httpVueLoader('./vue/vkey.vue') }
     ]
 })
 
